e11e808b82a11a0c3ac91ad1c73f59c54dc3b9dc
DiegoFranco16 <diego.alejandrof@hotmail.com>
2024-11-14 13:55:41 -0500

    Se guardan todas las transacciones realizadas en un archivo .json existente. Si no existe, lo crea

--- src/GeneradorDeArchivo.java
+++ src/GeneradorDeArchivo.java
@@ -1,16 +1,38 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GeneradorDeArchivo {
-
+    private static final String FILE_NAME = "Historial_conversion_de_monedas.json";
     public void guardarJson(ConversionMoneda conversionMoneda) throws IOException {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        FileWriter escritura = new FileWriter("ConversionDeMonedas" + ".json");
-        //FileWriter escritura = new FileWriter(pelicula.title() + ".json");
-        escritura.write(gson.toJson(conversionMoneda));
+        List<ConversionMoneda> historialTransacciones = new ArrayList<>();
+        // Verificar si el archivo existe
+        File archivo = new File(FILE_NAME);
+        if (archivo.exists()) {
+            // Leer las transacciones existentes en el archivo
+            FileReader lector = new FileReader(archivo);
+            Type tipoLista = new TypeToken<List<ConversionMoneda>>(){}.getType();
+            historialTransacciones = gson.fromJson(lector, tipoLista);
+            lector.close();
+        }
+        // Agregar la nueva transacción a la lista
+        historialTransacciones.add(conversionMoneda);
+        // Guardar la lista actualizada en el archivo
+        FileWriter escritura = new FileWriter(FILE_NAME);
+        gson.toJson(historialTransacciones, escritura);
         escritura.close();
+
+//        FileWriter escritura = new FileWriter("Historial conversion de monedas" + ".json");
+//        escritura.write(gson.toJson(conversionMoneda));
+//        escritura.close();
     }
 }
